ff5939ad6bc2166d1579cae73b3352576abb1102
Roberto Toribio <roberttoribio04@gmail.com>
2023-05-30 16:19:11 -0700

    Finished exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,6 +2,7 @@ const Header = (props) => {
 /*
   This component is in charge of rendering the name of the course
 */
+console.log(props)
 return (
 <h1>
   {props.coursename}
@@ -28,13 +29,16 @@ const Total = (props) => {
 /*
 renders the number of exercises
 */
-console.log(props)
+console.log("from Total Component")
+console.log(props.totalEx[0].exercises)
+let total = props.totalEx[0].exercises + props.totalEx[1].exercises + props.totalEx[2].exercises
 return(
-  <p>Number of exercises: {props.totalEx}</p>
+  <p>Number of exercises: {total}</p>
 )
 }
 
 const Part = (props) => {
+  console.log(props)
   return(
     <p>
       {props.data.name} {props.data.exercises}
@@ -43,19 +47,22 @@ const Part = (props) => {
 }
 
 const App = () => {
-  const course = "Half  Stack application development"
-  const parts = [
+  const course ={ 
+    name: "Half  Stack application development",
+    
+    parts: [
     {name:"Fundamentals of React", exercises: 10},
     {name:"Using props to pass data", exercises:7},
     {name:"State of a component", exercises:14}
-
-  ]
-  const totalEx = parts[0].exercises + parts[1].exercises + parts[2].exercises
+    ]
+  }
+  
+  //const totalEx = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return (
     <div>
-     <Header coursename={course}/>
-     <Content contentData={parts}/>
-     <Total totalEx= {totalEx} />
+     <Header coursename={course.name}/>
+     <Content contentData={course.parts}/>
+     <Total totalEx= {course.parts} />
     </div>
   )
 }
