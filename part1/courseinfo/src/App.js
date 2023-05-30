ca8a14f5f9c5b81929f17100408f41265f8696b5
Roberto Toribio <roberttoribio04@gmail.com>
2023-05-30 15:58:57 -0700

    Refactored code as per exercise 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,6 +13,7 @@ const Content = (props) => {
   This component is in charge of rendering the parts andd their number
   of exercises
   */
+ console.log(props)
  return (
   <div>
     <Part data={props.contentData[0]} />
@@ -35,27 +36,40 @@ return(
 const Part = (props) => {
   return(
     <p>
-      {props.data.part} {props.data.numEx}
+      {props.data.name} {props.data.exercises}
     </p>
   )
 }
 
 const App = () => {
-  const course = "Half  Stack application development"
-  
-
-  const contentData = [
+  //const course = "Half  Stack application development"
+  /*const contentData = [
     {part:"Fundamentals of React", numEx: 10},
     {part:"Using props to pass data", numEx:7},
     {part:"State of a component", numEx:14}
 
   ]
-  const totalEx = contentData[0].numEx + contentData[1].numEx + contentData[2].numEx
+  const totalEx = contentData[0].numEx + contentData[1].numEx + contentData[2].numEx*/
 
+
+  const course = 'Half stack application development'
+  const part1 ={
+    name:"Fundamentals of React",
+    exercises:10
+  }
+  const part2 = {
+    name:"Using props to pass data",
+    exercises: 7
+  }
+  const part3 = {
+    name:"State of a component",
+    exercises: 14
+  }
+  const totalEx = part1.exercises + part2.exercises + part3.exercises
   return (
     <div>
      <Header coursename={course}/>
-     <Content contentData={contentData}/>
+     <Content contentData={[part1,part2,part3]}/>
      <Total totalEx= {totalEx} />
     </div>
   )
