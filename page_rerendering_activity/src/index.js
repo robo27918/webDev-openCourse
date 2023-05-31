5b7da50994c32dcca03002152c37f04f64336680
Roberto Toribio <roberttoribio04@gmail.com>
2023-05-31 12:57:01 -0700

    Finished exercise

--- page_rerendering_activity/src/index.js
+++ page_rerendering_activity/src/index.js
@@ -2,4 +2,20 @@ import React from 'react'
 import ReactDOM  from 'react-dom/client'
 import App from './App'
 
-let cou
+
+let counter =  1
+const refresh = () => {
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App counter = {counter}/>
+)
+}
+//refresh()
+//counter +=1
+//refresh()
+//counter +=1
+//refresh()
+
+setInterval(() => {
+  refresh()
+  counter+=1
+}, 3000)
