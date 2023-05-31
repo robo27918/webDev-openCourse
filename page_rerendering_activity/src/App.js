cb247f0abb1b3b63863e3f760c7a1c0ba6dbbe52
Roberto Toribio <roberttoribio04@gmail.com>
2023-05-31 13:22:29 -0700

    Finished event handler stuff

--- page_rerendering_activity/src/App.js
+++ page_rerendering_activity/src/App.js
@@ -1,7 +1,27 @@
-const App = (props) => {
-  const {counter} = props
+import {useState} from 'react'
+const App = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  const increseByOne = () => setCounter(counter+1)
+  const setToZero = () => setCounter(0)
+  /***
+  setTimeout(
+    ()=> setCounter(counter +1),
+    1000
+  )**/
+  console.log("rendering...", counter)
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={increseByOne}>
+        Plus ++
+      </button>
+      
+      <button onClick={setToZero}>
+        zero
+      </button>
+      </div>
   )
 }
 
