8c7d53d1ff3cc388c56adb4676ec861171529e04
Roberto Toribio <roberttoribio04@gmail.com>
2023-05-31 16:01:08 -0700

    Finshed section on conditional rendering

--- part1dactivity/src/App.js
+++ part1dactivity/src/App.js
@@ -1,32 +1,58 @@
 import {useState} from 'react'
 
-  
-
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left:0,
-    right:0
-  })
-
-  const [allClicks, setAll] = useState([])
-  
-  const handleLeftClick = () => 
-    setClicks({...clicks, left: clicks.left +1})
-  
-
-  const handleRightClick = () => 
-    setClicks({...clicks, right: clicks.right +1})
-  
+const History = (props) => {
+if(props.allClicks.length === 0){
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      {clicks.right}
+      the app is used by pressing the buttons
+    </div>
+  )
+}
+return (
+  <div>
+    button press history: {props.allClicks.join('')}
+  </div>
+)
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick= {handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [left,setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+  
+  const handleLeftClick = () => {
+    setAll (allClicks.concat('L'))
+    console.log("left before", left)
+    const updateLeft = left + 1
+    setLeft(updateLeft)
+    console.log("left after", updateLeft)
+    setTotal(updateLeft +right)
+
+
+  }
+  
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    console.log("right before", right)
+    const updatedRight = right +1
+    setRight(updatedRight)
+    console.log("right afer",updatedRight)
+    setTotal(left+updatedRight)
+  }
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left'/>
+      <Button handleClick={handleRightClick} text='right'/>
+      {right}
+      <History allClicks={allClicks}/>
     </div>
   )
   
