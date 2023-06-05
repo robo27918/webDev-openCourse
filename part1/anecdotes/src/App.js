7cbb18a3e69d356e23ca7c463bdc8e301f81ee63
Roberto Toribio <roberttoribio04@gmail.com>
2023-06-05 16:46:26 -0700

    Finished exercise 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,10 +1,37 @@
 import { useState } from 'react'
+const PopularVote = ({anecdotes,votes}) =>
+{
+  //how to count the total number of votes
+  let tot = 0
+  for (const key in votes){
+      tot += votes[key]
 
+  }
+  console.log("printing tot",tot)
+  const mostVoted = findMostVotes(votes)
+  console.log(mostVoted)
+  if (tot == 0){
+  return(
+    "No votes yet"
+   
+  )}
+  else{
+     return(anecdotes[mostVoted])
+  }
+  
+}
 const Button =({handleClick,text}) =>(
   <button onClick={handleClick}>
     {text}
   </button>
 )
+
+const findMostVotes = (votes) => {
+  //function to get key with the most votes from the votes object
+  const mostVotes = Object.keys(votes).reduce((a,b) => votes[a]>votes[b]? a:b);
+  console.log("idx with most votes",mostVotes)
+  return mostVotes
+}
 const getRandomIndex=(max) => {
   return Math.floor(Math.random() * max);
 }
@@ -36,24 +63,29 @@ const App = () => {
     const cpyvotes = {...votes}
     cpyvotes[selected] +=1
     setVotes(cpyvotes)
-
+    findMostVotes(votes)
     
   }
   const [selected,setSelected] = useState(0)
   const [votes, setVotes] = useState({0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0,7:0})
   console.log("printing object init state", votes)
+  
 
 
   
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <br/>
       has {votes[selected]} votes
       <br/>
       <Button handleClick={handleVoteClick} text="vote"></Button>
       <Button handleClick={handleNextClick} text="next anecdote"></Button>
+      <h1>Anecdote with most votes</h1>
+      <PopularVote anecdotes={anecdotes} votes={votes}/>
+      
     </div>
   )
 }
