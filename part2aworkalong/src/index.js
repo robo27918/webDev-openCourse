8e3c8fd2147f585ccce348f3f8c9d4726c332152
Roberto Toribio <roberttoribio04@gmail.com>
2023-06-05 20:44:46 -0700

    finished workalong for section2

---
+++ part2aworkalong/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+const notes = [
+  {id:1,
+  content:"html is easy",
+  important: true},
+  {
+    id:2,
+    content:"Browser can execute only javscript",
+    important: false
+
+  },
+  {
+    id:3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+
+  },
+  {
+    id:4,
+    content:"This is a made up of list item",
+    important: true
+  }
+  
+]
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>)
